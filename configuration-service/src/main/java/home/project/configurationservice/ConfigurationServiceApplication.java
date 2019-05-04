7184af4be36912386337ba8af2e2a34f282f1711
kobzardanila@gmail.com <wiseNcrazyman13>
2019-05-04 16:24:07 +0300

    Set up configuration service to pull config from github

---
+++ configuration-service/src/main/java/home/project/configurationservice/ConfigurationServiceApplication.java
@@ -0,0 +1,17 @@
+package home.project.configurationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+@EnableConfigServer
+@RefreshScope
+public class ConfigurationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServiceApplication.class, args);
+	}
+
+}
