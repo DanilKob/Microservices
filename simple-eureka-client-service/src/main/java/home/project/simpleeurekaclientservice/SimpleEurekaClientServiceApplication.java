f4cc6ad28f125f8efbaa54e42e345cf3a02f9396
kobzardanila@gmail.com <wiseNcrazyman13>
2019-05-05 15:40:14 +0300

    init commit

---
+++ simple-eureka-client-service/src/main/java/home/project/simpleeurekaclientservice/SimpleEurekaClientServiceApplication.java
@@ -0,0 +1,13 @@
+package home.project.simpleeurekaclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEurekaClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEurekaClientServiceApplication.class, args);
+	}
+
+}
