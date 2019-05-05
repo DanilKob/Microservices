8c3d52471dd09a50ea833deb23f9832b845610ac
kobzardanila@gmail.com <wiseNcrazyman13>
2019-05-05 12:58:14 +0300

    Setup service to be eureka client

--- configuration-service/src/main/java/home/project/configurationservice/ConfigurationServiceApplication.java
+++ configuration-service/src/main/java/home/project/configurationservice/ConfigurationServiceApplication.java
@@ -7,6 +7,13 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableConfigServer
+/*
+annotation
+will only reload the custom Spring properties you have in your application configura-
+tion. Items such as your database configuration that are used by Spring Data won’t be
+reloaded by the @RefreshScope annotation. To perform the refresh, you can hit the
+http://<yourserver>:8080/refresh endpoint.
+ */
 @RefreshScope
 public class ConfigurationServiceApplication {
 
