c403a0a500ac8656da597e2b2ab6d642b852c43e
kobzardanila@gmail.com <wiseNcrazyman13>
2019-05-12 23:48:40 +0300

    add second instance startup of cinfig service

--- service-startup-scripts/configuration-service-clusteres-instatnces.sh
+++ service-startup-scripts/configuration-service-clusteres-instatnces.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 gnome-terminal -- java -jar -Dspring.profiles.active=clustered-instance1 configuration-service-0.0.1-SNAPSHOT.jar
+gnome-terminal -- java -jar -Dspring.profiles.active=clustered-instance2 configuration-service-0.0.1-SNAPSHOT.jar
