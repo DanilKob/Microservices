12da3534fce1d534fb04ff93f2248a89354b07f2
kobzardanila@gmail.com <wiseNcrazyman13>
2019-06-04 01:34:54 +0300

    setup resource server security

--- booking-app/src/main/java/home/project/bookingapp/BookingAppApplication.java
+++ booking-app/src/main/java/home/project/bookingapp/BookingAppApplication.java
@@ -3,9 +3,11 @@ package home.project.bookingapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableResourceServer
 public class BookingAppApplication {
 
 	public static void main(String[] args) {
